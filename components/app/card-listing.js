ea46b5a0116a25ffc5a169a0ad05476be14497e1
tfs <tiagofsanchez@gmail.com>
2024-03-25 23:58:14 +0000

    SitePage: subdomain is connected and I can have all the data in the Site Page. Every link is already connected to it

--- components/app/card-listing.js
+++ components/app/card-listing.js
@@ -1,6 +1,10 @@
 import Link from "next/link";
 
 const CardListing = ({ id, name, subdomain }) => {
+  const currentHost =
+    process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
+      ? `https://${subdomain}.brokertree.co`
+      : `http://${subdomain}.localhost:3000`;
   return (
     <div className="relative rounded-lg border border-stone-200 pb-10 shadow-md transition-all hover:shadow-xl dark:border-stone-700 dark:hover:border-white">
       <Link href={`/listing/${id}`}>
@@ -13,11 +17,16 @@ const CardListing = ({ id, name, subdomain }) => {
         </p> */}
         </div>
       </Link>
-      <div className="absolute bottom-4 flex w-full justify-between space-x-4 px-4">
+      <Link
+        href={currentHost}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="absolute bottom-4 flex w-full justify-between space-x-4 px-4"
+      >
         <div className="truncate rounded-md bg-stone-100 px-2 py-1 text-sm font-medium text-stone-600 transition-colors hover:bg-stone-200 dark:bg-stone-800 dark:text-stone-400 dark:hover:bg-stone-700">
-          {subdomain} ↗
+          {subdomain}.{process.env.NEXT_PUBLIC_ROOT_DOMAIN} ↗
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
--- pages/app/listing/[id]/index.js
+++ pages/app/listing/[id]/index.js
@@ -1,21 +1,38 @@
 import Layout from "@/components/app/Layout";
+import { Separator } from "@/components/ui/separator";
 import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
+import Link from "next/link";
 
 // TODOS:
+
+// DONE: Header for the page in order to have the page with the proper name in the page header
+// DONE: Need to link to the page that the user actually built
 // Show all guides
-// Header for the page in order to have the page with the proper name in the page header
 // Guides section with all the guides that the user will have been putting together
 
 const ListingPage = ({ listingData }) => {
+  const currentHost =
+    process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
+      ? `https://${listingData.subdomain}.brokertree.co`
+      : `http://${listingData.subdomain}.localhost:3000`;
   return (
     <Layout>
-      <div className="p-5 space-y-8 wrap">
-        <div className="flex gap-3 items-end">
-          <h1 className="text-3xl font-extrabold ">{listingData.name}</h1>
-          <p className="truncate rounded-md bg-stone-100 px-2 py-1 text-sm font-medium text-stone-600 transition-colors hover:bg-stone-200 dark:bg-stone-800 dark:text-stone-400 dark:hover:bg-stone-700">
-            {listingData.subdomain} ↗
-          </p>
+      <div className="p-5">
+        <div className="space-y-2">
+          <h1 className="text-3xl font-extrabold capitalize ">
+            {listingData.name}
+          </h1>
+          <Link
+            href={currentHost}
+            target="_blank"
+            rel="noopener noreferrer"
+            className=" inline-flex truncate rounded-md bg-stone-100 px-2 py-1 text-sm font-medium text-stone-600 transition-colors hover:bg-stone-200 dark:bg-stone-800 dark:text-stone-400 dark:hover:bg-stone-700"
+          >
+            {listingData.subdomain}.{process.env.NEXT_PUBLIC_ROOT_DOMAIN} ↗
+          </Link>
         </div>
+        <Separator className="mt-4" />
+        <h2 className=" text-lg mt-5">Existing guides guides</h2>
       </div>
     </Layout>
   );
