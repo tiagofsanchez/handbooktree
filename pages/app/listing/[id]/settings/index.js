9241b71dbc57e5eeb0009d6789b72a75b85dfdaa
tfs <tiagofsanchez@gmail.com>
2024-03-26 18:09:56 +0000

    inputForm: was able to abstract away the form for inputs

--- pages/app/listing/[id]/settings/index.js
+++ pages/app/listing/[id]/settings/index.js
@@ -1,15 +1,32 @@
 import Layout from "@/components/app/Layout";
+import InputForm from "@/components/form/inputForm";
+import * as Yup from "yup";
 
 // TODO
 // Need to create a for that will help with deleting
 // Need to create a for that will help with renaming the platform
 // The forms that I will be using here is the same form as in the other pieces
+const nameSchema = Yup.object().shape({
+  name: Yup.string().required("Required"),
+});
 
 const SettingsPage = () => {
   return (
     <Layout>
-      <div className="p-6 space-y-6">
-        <h1>Settings Page</h1>
+      <div className="p-6 space-y-8">
+        <h1 className="text-3xl font-extrabold">Settings Page</h1>
+        <div>
+          <InputForm
+            input="name"
+            inputValue="Tiago"
+            title="Name"
+            description="Redefine the name of your app"
+            placeholder="The name your app..."
+            helpMessage="This will be used for SEO."
+            buttonAction="Save changes"
+            validationSchema={nameSchema}
+          />
+        </div>
       </div>
     </Layout>
   );
