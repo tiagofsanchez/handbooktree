06c0a9e77c4992271326c16f2b43f25ff3f557af
tfs <tiagofsanchez@gmail.com>
2024-03-26 19:37:28 +0000

    ListingPage Settings: Input subdomain

--- pages/app/listing/[id]/settings/index.js
+++ pages/app/listing/[id]/settings/index.js
@@ -18,10 +18,13 @@ import * as Yup from "yup";
 const nameSchema = Yup.object().shape({
   name: Yup.string().required("Required"),
 });
+const subdomainSchema = Yup.object().shape({
+  subdomain: Yup.string().required("Required"),
+});
 
 const SettingsPage = ({ listingData, id }) => {
   const supabase = useSupabaseClient();
-  const router = useRouter()
+  const router = useRouter();
   const refreshData = () => {
     void router.replace(router.asPath, undefined, {
       scroll: false,
@@ -51,16 +54,27 @@ const SettingsPage = ({ listingData, id }) => {
     <Layout>
       <div className="p-5 ">
         <ListingPageHeader listingData={listingData} />
-        <div className="mt-5">
+        <div className="mt-5 grid gap-5">
           <InputForm
             input="name"
             inputValue={listingData.name}
             title="Name"
             description="Redefine the name of your app"
-            placeholder="The name your app..."
+            placeholder="The name of your app..."
             helpMessage="This will be used for SEO."
             buttonAction="Save changes"
             validationSchema={nameSchema}
+            updateSupabase={updateListingName}
+          />
+          <InputForm
+            input="subdomain"
+            inputValue={listingData.subdomain}
+            title="Subdomain"
+            description="Redefine the subdomain of your app"
+            placeholder="The subdomain of your app..."
+            helpMessage="Subdomain is the name you will use in the url."
+            buttonAction="Save changes"
+            validationSchema={subdomainSchema}
             updateSupabase={updateListingName}
           />
         </div>
