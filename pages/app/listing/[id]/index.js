a3114c8faf118ba688f0d548d97a49e45290026e
tfs <tiagofsanchez@gmail.com>
2024-03-27 14:44:51 +0000

    ListingPage: I have the CARD for the guides

--- pages/app/listing/[id]/index.js
+++ pages/app/listing/[id]/index.js
@@ -1,20 +1,40 @@
 import Layout from "@/components/app/Layout";
 import ListingPageHeader from "@/components/app/listinPageHeader";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { createPagesServerClient } from "@supabase/auth-helpers-nextjs";
 
 // TODOS:
 // DONE: Header for the page in order to have the page with the proper name in the page header
 // DONE: Need to link to the page that the user actually built
 // Show all guides
-// Guides in supabase
+// DONE: Guides in supabase
+// Guides UI 
+// What happens where there are no guides - show a no guide page
 // Guides section with all the guides that the user will have been putting together
 
-const ListingPage = ({ listingData }) => {
+const ListingPage = ({ listingData, guidesData }) => {
   return (
     <Layout>
       <div className="p-5">
         <ListingPageHeader listingData={listingData} />
-        <h2 className=" text-lg mt-5">Existing guides</h2>
+        <div className="grid gap-5 mt-5">
+          {guidesData.map((guide) => (
+            <Card
+              key={guide.id}
+              className="border border-stone-200  shadow-md transition-all hover:shadow-xl dark:border-stone-700 dark:hover:border-white"
+            >
+              <CardHeader>
+                <CardTitle>{guide.title}</CardTitle>
+                <CardDescription>{guide.description}</CardDescription>
+              </CardHeader>
+            </Card>
+          ))}
+        </div>
       </div>
     </Layout>
   );
@@ -29,9 +49,15 @@ export const getServerSideProps = async (ctx) => {
     .select("name, subdomain")
     .eq("id", id);
 
+  const { data: guides } = await supabase
+    .from("guides")
+    .select("id, title , description ")
+    .eq("listing_id", id);
+
   return {
     props: {
       listingData: data[0],
+      guidesData: guides,
     },
   };
 };
